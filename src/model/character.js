d9f4b1b9dcff002215bffd00f5fc781fd40910a1
RobertaSciortino <robertasciortino.ag@gmail.com>
2022-09-21 10:14:57 +0200

    Created character model and test

---
+++ src/model/character.js
@@ -0,0 +1,15 @@
+function character(id, name, avatar, species, gender, status, originLocation, lastLocation, episode) {
+    return {
+        id,
+        name,
+        avatar,
+        species,
+        gender,
+        status,
+        originLocation,
+        lastLocation,
+        episode
+    }
+}
+
+export default character;
