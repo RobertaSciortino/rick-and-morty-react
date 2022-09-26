0208ddd5e48f9e5b6f06d0287008931de3521751
RobertaSciortino <robertasciortino.ag@gmail.com>
2022-09-26 11:54:33 +0200

    Created loader

---
+++ src/components/Loader.js
@@ -0,0 +1,11 @@
+import classes from '../style/loader.module.css';
+
+const Loader = () => {
+    return (
+        <div className={classes.loader}>
+            <img src="cute-morty-smith-512x512.png"/>
+        </div>
+    )
+}
+
+export default Loader;
