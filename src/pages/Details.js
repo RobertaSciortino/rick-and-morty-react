a86cd0a306319d1d2efc588b40a1d436d0f0d66d
RobertaSciortino <robertasciortino.ag@gmail.com>
2022-09-29 19:19:22 +0200

    updated hooks and updated responsive detail layout

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -3,37 +3,39 @@ import LocationDetails from "../components/LocationDetails";
 import classes from '../style/details.module.css';
 import EpisodeDetails from "../components/EpisodeDetails";
 import { useDetails } from "../hooks/useDetails";
+import { useNavigate } from "react-router-dom";
 
 const Details = () => {
-    const details = useDetails();
+    const {character, lastLocationDetails, originLocationDetails, episodeList } = useDetails();
+    const navigate = useNavigate();
     
     return (
         <Container>
             <Row>
                 <Col md={{span: 10,  offset: 1}} lg={{span: 8, offset: 2}}>
-                    <Card bg="light" className={`${classes.card} border-0 d-md-flex flex-row`}>
-                        <Card.Img variant="start" src={details.character.avatar} className="rounded-start"/>
+                    <Card bg="light" className={`${classes.card} border-0 d-md-flex flex-md-row`}>
+                        <Card.Img src={character.avatar} className={classes['rounded-start']}/>
                         <Card.Body>
-                            <Card.Title className="text-uppercase title-details">{details.character.name}</Card.Title>
+                            <Card.Title className="text-uppercase title-details">{character.name}</Card.Title>
                             <ListGroup as="ul" className="font-14 mb-4">
                                 <ListGroup.Item as="li" className="rounded-0 border-top-0 border-start-0 border-end-0 ps-0 pe-0 text-overflow bg-light">
-                                    <strong className="text-uppercase">species:</strong> {details.character.species}
+                                    <strong className="text-uppercase">species:</strong> {character.species}
                                 </ListGroup.Item>
                                 <ListGroup.Item as="li" className="border-start-0 border-end-0 ps-0 pe-0 text-overflow bg-light">
-                                    <strong className="text-uppercase">gender:</strong> {details.character.gender}
+                                    <strong className="text-uppercase">gender:</strong> {character.gender}
                                 </ListGroup.Item>
                                 <ListGroup.Item as="li" className="rounded-0 border-start-0 border-end-0 ps-0 pe-0 text-overflow bg-light">
-                                    <strong className="text-uppercase">status:</strong> {details.character.status}
+                                    <strong className="text-uppercase">status:</strong> {character.status}
                                 </ListGroup.Item>
                             </ListGroup>                          
                             
-                            <LocationDetails title="Last known location" data={details.lastLocationDetails} />
-                            <LocationDetails title="First seen in" data={details.originLocationDetails} />
+                            <LocationDetails title="Last known location" data={lastLocationDetails} />
+                            <LocationDetails title="First seen in" data={originLocationDetails} />
 
-                            <EpisodeDetails title="Appeared on" data={details.episodeList} />
+                            <EpisodeDetails title="Appeared on" data={episodeList} />
 
                             <div className="text-end">
-                                <Button variant="primary" className="text-uppercase border-0 mt-5" onClick={() => details.navigate('/')}>Back</Button>
+                                <Button variant="primary" className="text-uppercase border-0 mt-5" onClick={() => navigate('/')}>Back</Button>
                             </div>
                             
                         </Card.Body>
